14b857b11158198681dd0af84f5fd833b89ca3c8
Tom Hallam <thomashallam@gmail.com>
2014-08-02 10:20:46 +1000

    fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-require faker
+require 'faker'
 puts "Seeding data"
 
 5.times{
   Post.create(
-title: Faker::Company.catch_phrase,
-body: Faker::Lorem.paragraph
+title: Faker::Company.catch_phrase.to_s,
+body: Faker::Lorem.paragraph.to_s
   )
 }
 
 Post.all.each do |post|
-  3.times{post.comments.create(body: Faker::Lorem.sentences)}
+  3.times{post.comments.create(body: Faker::Lorem.sentences.to_s)}
 end
 
 puts "Seeding complete"
