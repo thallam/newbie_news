31bff64920dc475da865b499e3d0b87152231200
Tom Hallam <thomashallam@gmail.com>
2014-09-23 16:44:59 +1000

    working but rspec test failing...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
 
-def show
-end
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+    @comments = @user.comments
+  end
+
 end
--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,12 +3,22 @@ require 'rails_helper'
 describe "Visiting profiles" do
   before do
     @user = sample_user
+    @post = post_without_user(user: @user)
+    @comment = Comment.create(user: @user, body: "A Comment")
+    allow(@comment).to receive(:sent_favorite_emails)
+    @comment.save
   end
 
   describe "not signed in" do
     it "shows profile" do
       visit user_path(@user)
-      expect(current_path).to eq(user_path(+@user))
+      puts user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+
+      expect(page).to  have_content(@user.name)
+      expect(page).to  have_content(@comment.body)
+      expect(page).to  have_content(@post.title)
     end
 
   end
