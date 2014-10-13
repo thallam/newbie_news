7fcab26accd65cdc5ede938af7b46365dbd625c6
Tom Hallam <thomashallam@gmail.com>
2014-10-13 20:06:16 +1100

    added unobtrusive javascript delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,31 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
 
-def show
-end
-
-def new
-  @post = Post.find(params[:post_id])
-  authorize @comment
-end
-
-def create
-  @post = Post.find(params[:post_id])
-  @comment = current_user.comments.build(comment_params.merge(post_id: params[:post_id]))
-  authorize @comment
-
-  if @comment.save
-    flash[:notice] = "Comment created."
-    redirect_to topic_post_path(@post.topic_id, @post )
-  else
-    flash[:alert] = "There was an error <br> #{@comment.errors.to_a}"
-    redirect_to topic_post_path(@post.topic_id, @post )
+  def show
   end
 
-end
+  def new
+    @post = Post.find(params[:post_id])
+    authorize @comment
+  end
 
-def update
-end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.build(comment_params.merge(post_id: params[:post_id]))
+    authorize @comment
+
+    if @comment.save
+      flash[:notice] = "Comment created."
+      redirect_to topic_post_path(@post.topic_id, @post )
+    else
+      flash[:alert] = "There was an error <br> #{@comment.errors.to_a}"
+      redirect_to topic_post_path(@post.topic_id, @post )
+    end
+
+  end
+
+  def update
+  end
 
   def destroy
     @post = Post.find(params[:post_id])
@@ -34,18 +35,22 @@ end
 
     if @comment.destroy
       flash[:notice] = "The comment was destroyed"
-      redirect_to topic_post_path(@post.topic_id, @post )
+
     else
       flash[:error] = "There was an error destroying the comment"
-      redirect_to topic_post_path(@post.topic_id, @post )
+
     end
+
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
+    end
+    end
+
+
+    protected
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+
+
   end
-
-
-  protected
-  def comment_params
-    params.require(:comment).permit(:body)
-  end
-
-
-end
